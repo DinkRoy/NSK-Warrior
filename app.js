aa034ad23f5290bf49f146e524c34d73a37d805f
DinkRoy <130787450+DinkRoy@users.noreply.github.com>
2024-11-22 07:43:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+      if (navigator.serviceWorker.controller) {
+        console.log('Service worker installed');
+      }
+        navigator.serviceWorker.oncontrollerchange = (e) => {
+          console.log('New service worker activated');
+        };
+  });
+}
+
 window.addEventListener('popstate', async () => {
     console.log('Saving state before navigating back');
     await saveState();
     window.history.back();
-});
+})
