1bc27d28f2e70e163be4a6be5ef804f684a56f7c
DinkRoy <130787450+DinkRoy@users.noreply.github.com>
2025-10-08 19:29:18 -0500

    Update service-worker.js
    Added file paths to precache

--- service-worker.js
+++ service-worker.js
@@ -4,17 +4,49 @@ const networkFirstFiles = [
     '/index.html',
     '/app.js',
     '/scripts/auto-save-load/auto-save-load.js',
+    '/scripts/auto-save-load/auto-save-load.css',
     '/manifest.json',
     '/scripts/gamepad.js'
 ];
 
+// Precache for offline
 const urlsToCache = [
     '/',
     '/booklet/booklet.css',
     '/booklet/booklet.js',
     '/booklet/panzoom.min.js',
     '/booklet/turn.min.js',
-    'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js',
+    '/booklet/manual_icon.webp',
+    'https://nsk-warrior-kf-files.netlify.app/images/title.avif',
+    '/booklet/sounds/page_turn.mp3',
+    '/booklet/sounds/slide_in.mp3',
+    '/booklet/sounds/slide_out.mp3',
+    '/booklet/pages/1.webp',
+    '/booklet/pages/2.webp',
+    '/booklet/pages/3.webp',
+    '/booklet/pages/4.webp',
+    '/booklet/pages/5.webp',
+    '/booklet/pages/6.webp',
+    '/booklet/pages/7.webp',
+    '/booklet/pages/8.webp',
+    '/booklet/pages/9.webp',
+    '/booklet/pages/10.webp',
+    '/booklet/pages/11.webp',
+    '/booklet/pages/12.webp',
+    '/booklet/pages/13.webp',
+    '/booklet/pages/14.webp',
+    '/booklet/pages/15.webp',
+    '/booklet/pages/16.webp',
+    '/booklet/pages/17.webp',
+    '/booklet/pages/18.webp',
+    '/booklet/pages/19.webp',
+    '/booklet/pages/20.webp',
+    '/emulatorjs/4.0.9/data/',
+    '/emulatorjs/4.0.9/data/loader.js',
+    'https://nsk-warrior-kf-files.netlify.app/RPG Maker (USA).zip',
+    'https://nsk-warrior-kf-files.netlify.app/scph5501.bin',
+    'https://nsk-warrior-kf-files.netlify.app/RPG Maker (USA).state'
 ];
 
 self.addEventListener('install', event => {
